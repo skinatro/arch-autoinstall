39bf35708e90e576dae8f3594b3504333514f221
skinatro <60423203+skinatro@users.noreply.github.com>
2021-06-20 01:32:30 +0530

    Update install_script.sh

--- install_script.sh
+++ install_script.sh
@@ -1,42 +1,60 @@
-sudo timedatectl set-ntp true
-sudo mkfs.btrfs /dev/nvme0n1p6
-sudo mkswap /dev/nvme0n1p5
-sudo mount /dev/nvme0n1p6 /mnt
-sudo swapon /dev/nvme0n1p5 
-sudo mkfs.ext4 /dev/nvme0n1p4
-
-echo " manage sudoers thingy"
-
-sudo pacstrap /mnt base linux linux-firmware linux-headers dkms ntfs-3g git base-devel grub efibootmgr dosfstools os-prober mtools bc nvim amd-ucode NetworkManager
-sudo genfstab -U /mnt >> /mnt/etc/fstab
-sudo arch-chroot /mnt 
-sudo ln -sf /usr/share/zoneinfo/Asia/Calcutta /etc/localtime
-sudo hwclock --systohc
-
-echo "edit /etc/locale.gen then run locale-gen"
-
-sudo touch /etc/hostname 
-sudo echo kuroneko >> /etc/hostname
-sudo cat /etc/hostname
-sudo echo "127.0.0.1		localhost" >> /etc/hosts
-sudo echo "::1		locathost" >> /etc/hosts
-sudo echo "127.0.1.1		kuroneko.localdomain kuroneko" >> /etc/hosts
-sudo cat /etc/hosts
 
 
-#mkinitcpio -P
+
+timedatectl set-ntp true
+
+
+#file system stuff 
+#mkfs.btrfs /dev/nvme0n1p6
+#mkswap /dev/nvme0n1p5
+#mount /dev/nvme0n1p6 /mnt
+#swapon /dev/nvme0n1p5 
+#mkfs.fat -F32 /dev/nvme0n1p4
+
+
+
+
+pacstrap /mnt base sudo linux-zen linux-zen-headers linux-firmware dkms ntfs-3g git base-devel grub efibootmgr dosfstools os-prober mtools bc nvim amd-ucode NetworkManager
+genfstab -U /mnt >> /mnt/etc/fstab
+arch-chroot /mnt 
+ln -sf /usr/share/zoneinfo/Asia/Calcutta /etc/localtime
+hwclock --systohc
+
+#uncomment sudoers 
+sed -i '/#.*members.*wheel/{n;s/^#//}' /etc/sudoers
+
+echo 'en_US.UTF-8' >> /etc/locale.conf
+locale-gen 
+
+touch /etc/hostname 
+echo kuroneko >> /etc/hostname
+cat /etc/hostname
+cat >> /etc/hosts << EOF 
+
+127.0.0.1		localhost
+::1		locathost
+127.0.1.1		kuroneko.localdomain kuroneko
+EOF
+cat /etc/hosts
+
+
+mkinitcpio -p linux-zen
 passwd root
-sudo useradd -m kuroneko
+useradd -m kuroneko
 passwd kuroneko
-sudo usermod -aG wheel,audio,video,optical,storage kuroneko
+usermod -aG wheel,audio,video,optical,storage kuroneko
 
-sudo mkdir /boot/EFI
-sudo mount /dev/nvme0n1p4 /boot/EFI
-sudo grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
-sudo grub-mkconfig -o /boot/grub/grub.cfg
+mkdir /boot/EFI
+mount /dev/nvme0n1p4 /boot/EFI
+grub-install --target=x86_64-efi  --bootloader-id=grub_uefi --recheck
+grub-mkconfig -o /boot/grub/grub.cfg
 
+cd /home/kuroneko
+wget https://raw.githubusercontent.com/skinatro/arch-autoinstall/main/post_install.sh
+wget https://raw.githubusercontent.com/skinatro/arch-autoinstall/main/config_install.sh
+chmod +x post_install.sh
+chmod +x config_install.sh
 
-
-echo "do the sudoers file stuff and the locale-gen thingy"
-echo "will automate this stuff in the future"
 echo "also exit chroot by typing 'exit' and unmount /mnt by 'umount /mnt' then reboot"
+
+
